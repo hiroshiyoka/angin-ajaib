8ff3879d847ebd5cf1698a16d75838bd9cfd50fc
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-29 18:09:18 +0700

    feat(home): add HomeScreen component

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
