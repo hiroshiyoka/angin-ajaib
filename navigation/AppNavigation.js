dea65ec7df2ae2894d3ecc60eb34180a8ea4491e
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-29 18:12:49 +0700

    feat(navigation): integrate HomeScreen component into navigation stack

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LogBox, Text, View } from "react-native";
+import HomeScreen from "../screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,11 @@ const AppNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
