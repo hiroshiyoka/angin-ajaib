da1d7b7a1c3477633416634b73c3e3363215884d
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-30 12:20:46 +0700

    feat(home): enhance HomeScreen with search functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, SafeAreaView, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   return (
@@ -10,6 +17,20 @@ const HomeScreen = () => {
         className="absolute h-full w-full"
         blurRadius={70}
       />
+      <SafeAreaView className="flex flex-1">
+        <View style={{ height: "7%" }} className="mx-4 relative z-50">
+          <View className="flex justify-end items-center rounded-full">
+            <TextInput
+              className="pl-6 pb-1 h-10 flex-1 text-base text-white"
+              placeholder="Search City"
+              placeholderTextColor={"lightgray"}
+            />
+            <TouchableOpacity className="rounded-full p-3 m-1">
+              <MagnifyingGlassIcon size="25" color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
