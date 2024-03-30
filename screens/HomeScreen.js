e2ac429317e07873ec2d76db8d40ec4c29e77988
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-30 12:30:03 +0700

    feat(home): implement toggle functionality for search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,10 +6,13 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { useState } from "react";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import { theme } from "../theme";
 
 const HomeScreen = () => {
+  const [showSearch, toggleSearch] = useState(false);
+
   return (
     <View className="flex-1 relative">
       <StatusBar style="light" />
@@ -24,12 +27,15 @@ const HomeScreen = () => {
             className="flex justify-end items-center rounded-full"
             style={{ backgroundColor: theme.bgWhite(0.2) }}
           >
-            <TextInput
-              className="pl-6 pb-1 h-10 flex-1 text-base text-white"
-              placeholder="Search City"
-              placeholderTextColor={"lightgray"}
-            />
+            {showSearch ? (
+              <TextInput
+                className="pl-6 pb-1 h-10 flex-1 text-base text-white"
+                placeholder="Search City"
+                placeholderTextColor={"lightgray"}
+              />
+            ) : null}
             <TouchableOpacity
+              onPress={() => toggleSearch(!showSearch)}
               style={{ backgroundColor: theme.bgWhite(0.3) }}
               className="rounded-full p-3 m-1"
             >
