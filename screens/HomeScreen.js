460e9ecc8dda4e9dbf5e1fd19c3e3f87b6de946c
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-30 12:45:56 +0700

    feat(home): update location search results styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -49,10 +49,18 @@ const HomeScreen = () => {
           {locations.length > 0 && showSearch ? (
             <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
               {locations.map((location, index) => {
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? "border-b-2 border-b-gray-400"
+                  : "";
+
                 return (
                   <TouchableOpacity
                     key={index}
-                    className="flex-row items-center border-0 p-3 px-4 mb-1 border-b-2 border-b-gray-400"
+                    className={
+                      "flex-row items-center border-0 p-3 px-4 mb-1" +
+                      borderClass
+                    }
                   >
                     <MapPinIcon size="20" color="gray" />
                     <Text>Bandung, Indonesia</Text>
