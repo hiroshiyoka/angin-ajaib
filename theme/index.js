3b93dc9d80bc7d04df11eed06d2a29954c788f85
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-01 05:24:24 +0700

    feat(theme): add weatherImage object to theme

--- theme/index.js
+++ theme/index.js
@@ -1,3 +1,12 @@
 export const theme = {
   bgWhite: (opacity) => `rgba(255, 255, 255, ${opacity})`,
 };
+
+export const weatherImages = {
+  "Partly Cloudy": require("../assets/images/partlycloudy.png"),
+  "Heavy Rain": require("../assets/images/heavy-rain.png"),
+  "Heavy Rain at Times": require("../assets/images/heavy-rain.png"),
+  "Moderate or Heavy Freezing Rain": require("../assets/images/heavy-rain.png"),
+  "Moderate or Heavy Rain Shower": require("../assets/images/heavy-rain.png"),
+  "Moderate or Heavy Rain with thunder": require("../assets/images/heavy-rain.png"),
+};
