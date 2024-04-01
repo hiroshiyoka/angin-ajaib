e0e5bd0802b04f59d80f50243e9514f11842725b
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-02 04:25:55 +0700

    feat(theme): update weatherImage object in theme file

--- theme/index.js
+++ theme/index.js
@@ -4,6 +4,14 @@ export const theme = {
 
 export const weatherImages = {
   "Partly Cloudy": require("../assets/images/partlycloudy.png"),
+  "Moderate Rain": require("../assets/images/moderaterain.png"),
+  "Patchy Rain Possible": require("../assets/images/moderaterain.png"),
+  Sunny: require("../assets/images/sun.png"),
+  Clear: require("../assets/images/sun.png"),
+  Overcast: require("../assets/images/cloudy.png"),
+  Cloudy: require("../assets/images/cloudy.png"),
+  "Light Rain": require("../assets/images/moderaterain.png"),
+  "Moderate Rain at Times": require("../assets/images/moderaterain.png"),
   "Heavy Rain": require("../assets/images/heavy-rain.png"),
   "Heavy Rain at Times": require("../assets/images/heavy-rain.png"),
   "Moderate or Heavy Freezing Rain": require("../assets/images/heavy-rain.png"),
