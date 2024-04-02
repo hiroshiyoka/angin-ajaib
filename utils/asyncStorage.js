8bcc5d61ee2c9aff477014829d91503b4fef19c2
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-03 06:14:28 +0700

    feat(utils): add function to retrieve data from AsyncStorage

--- utils/asyncStorage.js
+++ utils/asyncStorage.js
@@ -7,3 +7,12 @@ export const storeData = async (key, value) => {
     console.log("Error storing value: ", error);
   }
 };
+
+export const getData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (error) {
+    console.log("Error retrieving value: ", error);
+  }
+};
